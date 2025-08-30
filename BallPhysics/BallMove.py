170a2e8fab51893e124b30fe28c4eae679e3faf8
Ahilan <akrandom782@gmail.com>
2025-08-30 13:50:24 -0700

    Added Visual Developments

--- BallPhysics/BallMove.py
+++ BallPhysics/BallMove.py
@@ -11,8 +11,8 @@ x0 = 0 # Start at Origin
 v0 = 5 # m/s right
 radius = 0.5 #m , Radius of Ball
 
-wall = 10 #m. 
-wall2 = -2 # m
+wall_right= 10 #m. 
+wall_left = -2 # m
 
 T = 60 # s, total time
 dt = 0.02 # s, Sampling time
@@ -21,7 +21,7 @@ N = int(T/dt) # How many times the Function should run
 
 xs = []
 for _ in range(N):
-    if ((x0+radius) >= wall) or ((x0-radius) <= wall2):
+    if ((x0+radius) >= wall_right ) or ((x0-radius) <= wall_left):
         v0 = -v0
     
     x0 = x0 + v0*dt
@@ -34,12 +34,20 @@ for _ in range(N):
 
 # ---------------- Figure: draw a real ball ----------------
 fig, ax = plt.subplots(figsize=(8, 2.6))
-ax.set_xlim(-5,11)
-ax.set_ylim(0,10)
+ax.set_xlim(wall_left -2 ,wall_right+2)
+ax.set_ylim(0,radius*3)
+ax.set_aspect('equal', adjustable='box') #  if not equal, the Output is sqiushed
+
+# Labels 
+ax.set_xlabel("x (m)")
+ax.set_yticks([])
+ax.set_title("Ball bouncing between two walls")
+
+
 
 # Draw Walls
-ax.axvline(x=wall)
-ax.axvline(x=wall2)
+ax.axvline(x=wall_right,color="Black",lw=3) # with 3
+ax.axvline(x=wall_left,color="Black",lw=3)
 
 ball = Circle(xy=[xs[0],radius],radius=radius)
 ax.add_patch(ball)

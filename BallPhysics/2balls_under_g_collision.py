9ccc93e9280182e69f6b374a7b46fd22f1dbbe23
Ahilan <akrandom782@gmail.com>
2025-08-30 17:53:19 -0700

    Added Left and Right Walls, and gave 2D Motion. Looks More Accurate now

--- BallPhysics/2balls_under_g_collision.py
+++ BallPhysics/2balls_under_g_collision.py
@@ -6,6 +6,8 @@ import webbrowser, os
 
 
 wall_below = 0 #m 
+wall_right = 25 #m
+wall_left = -5
 
 T = 60 # s, total time
 dt = 0.02 # s, Sampling time
@@ -14,6 +16,8 @@ N = int(T/dt) # How many times the Function should run
 
 # Modify drop_ball to include two ball collisions
 # Lets start with 1D Collison in y
+
+# Lets Add Horizontal Velocty and Make this Guys 2D and add walls on side
 
 A_y0 = 5 # Start at height above origin
 A_ys = [A_y0]
@@ -28,14 +32,18 @@ B_x0 = 0 # Start x at origin
 B_xs = [B_x0]
 B_radius = 0.5 #m , Radius of Ball
 
-A_v0 = 0 # m/s down
+A_vy0 = 0 # m/s down
 A_a = -9.81 #m/s^2, Gravity on Earth
+A_vx0 = 0
+
 A_r = 0.9 # coefficient of restitution, 
 # r=1: perfectly elastic → no energy loss → bounces forever.
 # r<1: inelastic → each bounce smaller.
 # r=0: perfectly inelastic → the ball sticks to the ground
-B_v0 = 0 # m/s down
+B_vy0 = 0 # m/s down
 B_a = -9.81 #m/s^2, Gravity on Earth
+
+B_vx0 = 0.002 #m/s right
 B_r = 0.9 # coefficient of restitution, 
 # r=1: perfectly elastic → no energy loss → bounces forever.
 # r<1: inelastic → each bounce smaller.
@@ -45,23 +53,27 @@ collision = False
 
 
 for i in range(N):
-    A_ds = A_v0*dt + (1/2)*A_a*(dt)**2
+    A_ds = A_vy0*dt + (1/2)*A_a*(dt)**2
     A_y0 = A_y0 + A_ds
+
+    A_x0 = A_x0 + A_vx0/dt
 
 # Seems like i have to update the velocities as well because otherwise, 
 # there just seems to a constant Velocity kinda of effect from acceleration
-    A_vf = A_v0 + A_a*dt
-    A_v0 = A_vf
+    A_vf = A_vy0 + A_a*dt
+    A_vy0 = A_vf
 
     A_ys.append(A_y0)
     A_xs.append(A_x0)
 
 
-    B_ds = B_v0*dt + (1/2)*B_a*(dt)**2
+    B_ds = B_vy0*dt + (1/2)*B_a*(dt)**2
     B_y0 = B_y0 + B_ds
 
-    B_vf = B_v0 + B_a*dt
-    B_v0 = B_vf
+    B_x0 = B_x0 + B_vx0/dt
+
+    B_vf = B_vy0 + B_a*dt
+    B_vy0 = B_vf
 
     B_ys.append(B_y0)
     B_xs.append(B_x0)
@@ -71,33 +83,59 @@ for i in range(N):
     # Each other
 
     # Lets Shorted this logic using net distance < Combined Radius
+    # Then Lets Make this a 2D collision
 
-    net_distance = abs(A_y0 - B_y0)
+    net_distance = np.sqrt((A_y0 - B_y0)**2 + (A_x0 - B_x0)**2)
     if net_distance < A_radius+B_radius:
-        A_v0 = -A_r * A_v0
-        B_v0 = -B_r * B_v0
+        A_vy0 = -A_r * A_vy0
+        B_vy0 = -B_r * B_vy0
+
+        A_vx0 = -A_r * A_vx0
+        B_vx0 = -B_r * B_vx0
+
+
 
     if (A_y0-A_radius) <= wall_below:
         collision = True
-        A_v0 = -A_r * A_v0
+        A_vy0 = -A_r * A_vy0
 
     if (B_y0-B_radius) <= wall_below:
         collision = True
-        B_v0 = -B_r * B_v0
+        B_vy0 = -B_r * B_vy0
+
+    if (A_x0 + A_radius) >= wall_right:
+        A_vx0 = -A_r * A_vx0
+
+    if (B_x0 + B_radius) >= wall_right:
+        B_vx0 = -B_r * B_vx0
+
+    if (A_x0 - A_radius) <= wall_left:
+        A_vx0 = -A_r * A_vx0
+
+    if (B_x0 - B_radius) <= wall_left:
+        B_vx0 = -B_r * B_vx0
+
     
 """
-A_xs,A_ys = drop_ball(x0=A_x0,y0=A_y0,v0=A_v0,radius=A_radius,a=A_a,r=A_r)
+A_xs,A_ys = drop_ball(x0=A_x0,y0=A_y0,v0=A_vy0,radius=A_radius,a=A_a,r=A_r)
 
-B_xs,B_ys = drop_ball(x0=B_x0,y0=B_y0,v0=B_v0,radius=B_radius,a=B_a,r=B_r)
+B_xs,B_ys = drop_ball(x0=B_x0,y0=B_y0,v0=B_vy0,radius=B_radius,a=B_a,r=B_r)
 
 """
 
 
 # ---------------- Figure: draw a real ball ----------------
-fig, ax = plt.subplots(figsize=(8, 6))
-ax.set_xlim(min(A_xs[0],B_xs[0])-5 ,max(A_xs[0],B_xs[0])+5)
+fig, ax = plt.subplots(figsize=(8, 7))
+#ax.set_xlim(min(A_xs[0],B_xs[0])-5 ,max(A_xs[0],B_xs[0])+5)
+ax.set_xlim(-10 ,30)
+
 ax.set_ylim(0,20)
 ax.set_aspect('equal', adjustable='box') #  if not equal, the Output is sqiushed
+
+
+# ALl walls
+ax.axvline(x=wall_left,color="Black",lw=3)
+ax.axvline(x=wall_right,color="Black",lw=3)
 
 # Labels 
 ax.set_xlabel("x (m)")
